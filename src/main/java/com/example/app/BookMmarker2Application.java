02fab82301f2f46df5dcc22abebe712d89eb711c
tahara <b.tahara0916@gmail.com>
2023-09-18 13:11:58 +0900

    BookMmarker2init

---
+++ src/main/java/com/example/app/BookMmarker2Application.java
@@ -0,0 +1,13 @@
+package com.example.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookMmarker2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookMmarker2Application.class, args);
+	}
+
+}
---
+++ src/main/java/com/example/app/domain/UserSns.java
@@ -0,0 +1,10 @@
+package com.example.app.domain;
+
+public class UserSns {
+
+	private Integer id;
+	private String snsName;
+	private String url;
+	private String AccountName;
+	private Integer userId;
+}
